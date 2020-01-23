4daad80d4b27e4c68696df8109a44a0ec886260c
Nicolas Almy <nicolas.almy@gmail.com>
2020-01-23 08:35:25 +0100

    Added missing ; in method signature in function getTemplateData

--- src/Widgets/CustomNavigationWidget.php
+++ src/Widgets/CustomNavigationWidget.php
@@ -7,7 +7,7 @@ use Ceres\Widgets\Helper\BaseWidget;
 class CustomNavigationWidget extends BaseWidget
 {
   protected $template = "CustomNavigationWidget::Widgets.NavigationWidget";
-  protected function getTemplateData($widgetSettings, $isPreview)
+  protected function getTemplateData($widgetSettings, $isPreview);
 }
 
 ?>
